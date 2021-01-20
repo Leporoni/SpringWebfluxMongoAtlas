9d846b572b5d3df46b813ff9aa122e66fe43698a
Leporoni <leporoni@hotmail.com>
2021-01-20 15:33:27 -0300

    Added method getPlaylistByEvents() to show how work non-blocking and asynchronous application

--- src/main/java/com/leporonitech/mongowebflux/controller/PlaylistController.java
+++ src/main/java/com/leporonitech/mongowebflux/controller/PlaylistController.java
@@ -3,11 +3,17 @@ package com.leporonitech.mongowebflux.controller;
 import com.leporonitech.mongowebflux.document.Playlist;
 import com.leporonitech.mongowebflux.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -26,5 +32,14 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> save(@RequestBody Playlist playlist) {
         return playlistService.save(playlist);
+    }
+
+    @GetMapping(value = "/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents(){
+        System.out.println("---Start get plalists by WEBFLUX--- " + LocalDateTime.now());
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(3));
+        Flux<Playlist> playlistFlux = playlistService.findAll();
+
+        return Flux.zip(interval, playlistFlux);
     }
 }
